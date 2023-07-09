d123ad942344fd6cd15dc3a442cf31fa0cfc9953
hoihoi0101 <chlrudgh5877@gmail.com>
2023-07-09 23:08:42 +0900

    4일차

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+#1009 정수론 문제..
+# import sys
+# input = sys.stdin.readline
+#
+# sqrList = [[10], [1], [2, 4, 8, 6], [3, 9, 7, 1], [4, 6],
+#            [5], [6], [7, 9, 3, 1], [8, 4, 2, 6], [9, 1]]
+#
+# for _ in range(int(input())):
+#     a, b = map(int, input().split())
+#     a %= 10
+#     ans = sqrList[a][b % len(sqrList[a]) - 1]
+#     print(ans)
+
 #1004
 # import sys
 # input = sys.stdin.readline
